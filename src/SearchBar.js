0956f1324d2b075bbdf0ff19c977309aba83b281
Nigel C <cheok.nigel@gmail.com>
2018-06-18 08:28:07 +0800

    responsiveness for smaller screens

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,6 +4,7 @@ class SearchBar extends Component {
     state = {
         query: '',
         searchResult: [],
+        // initialLoad: true,
     };
 
     hasClickedOnPlace = (location) => {
@@ -15,9 +16,12 @@ class SearchBar extends Component {
     };
 
     componentDidMount() {
+        // this.state.searchResult = this.props.locations;
+        this.setState({ searchResult: this.props.locations });
     };
 
     filterPlacesByQuery = (query) => {
+        // this.initialLoad = false;
         this.setState({query: query});
         if (query) {
             let searchResult = this.props.locations.filter(location => location.title.toLowerCase().includes(query.toLowerCase()));
@@ -37,19 +41,22 @@ class SearchBar extends Component {
                     <input type="text" placeholder="Search for places"
                            onChange={event => this.filterPlacesByQuery(event.target.value)}
                     />
-                    {this.state.query.length > 0 && this.state.searchResult.length > 0 &&
-                    <div className="search-nearest-title">Places matching your search: &nbsp;
+                    { (this.state.searchResult.length > 0) &&
+                    <div className="search-nearest-title">
+                        { this.state.query.length > 0 && <span>Places matching your search:</span> }
+                        { this.state.query.length === 0 && <span>All Places:</span> }
+                        &nbsp;
                         { this.state.searchResult.map((location, index) =>
-                            <div className="display-inline" key={index} onClick={this.hasClickedOnPlace.bind(this,location)}>
-                                <span className="search-nearest-title-link">{location.title}</span>
+                            <div className="display-inline search-nearest-title-link" key={index} onClick={this.hasClickedOnPlace.bind(this,location)}>
+                                <span>{location.title}</span>
                             </div>
                         )}
                     </div>
                     }
-                    {this.state.query.length > 0 && this.state.searchResult.length === 0 &&
+                    { this.state.searchResult.length === 0 && this.state.query.length > 0 &&
                     <div className="search-nearest-title">There are no results found for '{this.state.query}'.</div>
                     }
-                    {this.state.query.length === 0 &&
+                    { this.state.searchResult.length === 0 && this.state.query.length === 0 &&
                     <div className="search-nearest-title">Start typing in the bar above to search for places.</div>
                     }
                 </div>
